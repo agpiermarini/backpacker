92c0db3d08714a967cafa0b9897d5297d3f67f2f
agpiermarini <piermarini.andrew@gmail.com>
2018-04-01 23:51:45 -0600

    Add user controller and user#new

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  def new
+    # @user = User.new
+  end
+
+  # def create
+    #if password and password_confirmation match, user.new
+    #if user.save
+      # flash[:success] = "Welcome to Backpacker!"
+    #else
+      # render :new
+    # end
+  # end
+end
