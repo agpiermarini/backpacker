ac140939ab797f64ff3f96fae6346facc2e73316
agpiermarini <piermarini.andrew@gmail.com>
2018-04-02 20:56:52 -0600

    Add login functionality and nav bar partials with accompanying  helper_method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,18 @@ module ApplicationHelper
         when :error then "alert alert-error"
         when :alert then "alert alert-error"
     end
-end
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_path
+    request.path
+  end
+
+  def nav_bar
+    return "layouts/nav_neutral" if current_path == login_path
+    current_user ? "layouts/nav_user" : "layouts/nav_visitor"
+  end
 end
--- spec/features/user/user_authentication_spec.rb
+++ spec/features/user/user_authentication_spec.rb
@@ -23,18 +23,16 @@ describe 'User' do
 
   describe 'completes login form with incorrect username or password' do
     it 'and sign in fails' do
-      username = "new"
-      password = "password"
-      bad_password = "123456"
-      email = "email@email.com"
-      user = User.create!(username: username, password: password, email: email)
+      user = User.create!(username: "username", password: "password", email: "email@email.com")
 
       visit '/'
 
       click_on "Sign In"
 
-      fill_in 'username', with: username
-      fill_in 'password', with: bad_password
+      fill_in 'username', with: user.username
+      fill_in 'password', with: "bad_password"
+
+      
       click_on 'Sign In'
 
       expect(current_path).to eq(login_path)
