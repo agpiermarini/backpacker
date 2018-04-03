70fb067ea4f7a2d6db5948adc58be204fcaac7e8
agpiermarini <piermarini.andrew@gmail.com>
2018-04-02 18:55:12 -0600

    Add test for sign in failure

--- spec/features/user/user_authentication_spec.rb
+++ spec/features/user/user_authentication_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User' do
   describe 'completes login form' do
-    it 'successfully logs in' do
+    it 'successfully signs in' do
       username = "new"
       password = "password"
       email = "email@email.com"
@@ -18,6 +18,27 @@ describe 'User' do
 
       expect(current_path).to eq(user_path(user))
       expect(page).to have_content("Welcome, #{username}!")
+    end
+  end
+
+  describe 'completes login form with incorrect username or password' do
+    it 'and sign in fails' do
+      username = "new"
+      password = "password"
+      bad_password = "123456"
+      email = "email@email.com"
+      user = User.create!(username: username, password: password, email: email)
+
+      visit '/'
+
+      click_on "Sign In"
+
+      fill_in 'username', with: username
+      fill_in 'password', with: bad_password
+      click_on 'Sign In'
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Incorrect username and/or password")
     end
   end
 end
