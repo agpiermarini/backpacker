cece6986976a1fdcd605784ddf7eece9891ae53a
agpiermarini <piermarini.andrew@gmail.com>
2018-04-04 21:34:45 -0600

    Update to syle sheet

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Image, type: :model do
   describe 'validations' do
     it{should validate_presence_of(:name)}
-    it{should validate_uniqueness_of(:name)}
     it{should validate_presence_of(:url)}
     it{should validate_uniqueness_of(:url)}
   end
